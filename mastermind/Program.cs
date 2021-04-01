f78b513264fc0ea2013174101f22ac4ba4bea554
ORG4N <callumorgan@gmail.com>
2021-04-01 17:44:44 +0100

    Taking inputs for pattern size and range of numbers to guess from, and generating a random pattern

--- mastermind/Program.cs
+++ mastermind/Program.cs
@@ -4,8 +4,13 @@ namespace mastermind
 {
     class Program
     {
-        public enum States {START, PLAYING, QUIT};
+        public enum States {START, NUMBERS, POSITIONS, PLAYING, END, QUIT};
         public static States currentState = States.START;
+
+        public int numbersMax = 0;
+        public int positions = 0;
+
+        public int[] pattern = new int[0]; 
 
         static void Main(string[] args)
         {
@@ -16,30 +21,116 @@ namespace mastermind
             Console.WriteLine("Inputs:");
             Console.WriteLine(" 'rules' - to learn how to play");
             Console.WriteLine(" 'play'  - to start playing");
-            Console.WriteLine(" 'quit'  - to exit the program\n");
+            Console.WriteLine(" 'quit'  - to exit the program");
 
             while (currentState != States.QUIT)
             {
                 string input = app.readInput();
                 app.processInput(input);
+                app.appLoop();
+            }
+        }
+
+        public void appLoop()
+        {
+            if (currentState == States.START)
+            {
+                Console.WriteLine("Inputs:");
+                Console.WriteLine(" 'rules' - to learn how to play");
+                Console.WriteLine(" 'play'  - to start playing");
+                Console.WriteLine(" 'quit'  - to exit the program");
+            }
+
+            if (currentState == States.NUMBERS)
+            {
+                Console.WriteLine("\nRange of numbers   : 1 to -");
+                Console.WriteLine("Amount of positions: -\n");
+
+                Console.WriteLine("Choose the amount of possible NUMBERS for the pegs to be chosen from.");
+                Console.WriteLine(" - Input must be an integer between 1 and 8");
+            }
+
+            if (currentState == States.POSITIONS)
+            {
+                Console.WriteLine("\nRange of numbers   : 1 to {0}", numbersMax);
+                Console.WriteLine("Amount of positions: -\n");
+
+                Console.WriteLine("Choose the amount of POSITIONS within the secret pattern.");
+                Console.WriteLine(" - Input must be an integer greater than 1");
+            }
+
+            if (currentState == States.PLAYING)
+            {
+                Console.Clear();
+                Console.WriteLine("\nRange of numbers   : 1 to {0}", numbersMax);
+                Console.WriteLine("Amount of positions: {0}\n", positions);
+
+                pattern = new int[positions];
+                getPattern();
             }
         }
 
         // Take an input from the user and convert to lowercase
         public string readInput()
         {
-            Console.Write("Input here: ");
+            Console.Write("\nInput here: ");
             string input = Console.ReadLine();
+            Console.WriteLine("");
             return input.ToLower();
         }
 
 
-        // Use the input to determine if the program state needs to change
+        // Use the input to determine if the program should do anything (i.e. change a state, call a method)
         public void processInput(string input)
         {
             if (input == "rules" && currentState == States.START) { printRules(); }
-            if (input == "play" && currentState == States.START) { currentState = States.PLAYING; }
-            if (input == "quit" && currentState == States.START) { currentState = States.QUIT; }
+
+            else if (input == "quit") { currentState = States.QUIT; }
+
+            else if (input == "play" && currentState == States.START) 
+            {
+                currentState = States.NUMBERS;
+                Console.Clear();
+            }
+
+            else if (currentState == States.NUMBERS)
+            {
+                int num;
+                bool isNumber = Int32.TryParse(input, out num);
+
+                if (isNumber)
+                {
+                    if (num > 0 && num <= 8)
+                    {
+                        numbersMax = num;
+                        currentState = States.POSITIONS;
+                    }
+
+                    else { Console.WriteLine("\nINVALID INPUT - Input must be a number and between 1 and 8\n"); }
+
+                }
+
+                else {Console.WriteLine("\nINVALID INPUT - Input must be a number and between 1 and 8\n");}
+            }
+
+            else if (currentState == States.POSITIONS)
+            {
+                int num;
+                bool isNumber = Int32.TryParse(input, out num);
+
+                if (isNumber)
+                {
+                    if (num > 0)
+                    {
+                        positions = num;
+                        currentState = States.PLAYING;
+                    }
+
+                    else {Console.WriteLine("\nINVALID INPUT - Input must be a number and greater than 1\n");}
+                }
+
+                else {Console.WriteLine("\nINVALID INPUT - Input must be a number and greater than 1\n");}
+            }
         }
 
 
@@ -47,12 +138,12 @@ namespace mastermind
         public void printRules()
         {
             Console.WriteLine("\n----------------------------------------------------------------------------------------------------------------------------------");
-            Console.WriteLine(" - Player A will secretly select N pegs from a selection of M colours and position them in a set order to create a hidden pattern.");
+            Console.WriteLine(" - Player A will secretly select N pegs from a selection of M numbers and position them in a set order to create a hidden pattern.");
             Console.WriteLine(" - Player B will then make a series of guesses to try to find out Player A's pattern.\n");
 
             Console.WriteLine(" - After confirming their turn, Player B will be able to see which of their guesses are correct via black and white pegs.");
-            Console.WriteLine("    - Black: position and colour are both correct");
-            Console.WriteLine("    - White: position is incorrect, but the colour exists somewhere within the pattern\n");
+            Console.WriteLine("    - Black: position and number are both correct");
+            Console.WriteLine("    - White: position is incorrect, but the number exists somewhere within the pattern\n");
 
 
             Console.WriteLine(" - The program will take the role of Player A, and therefore the user is Player B.");
@@ -60,6 +151,24 @@ namespace mastermind
             Console.WriteLine("    a. The user forfeits or exits the program.");
             Console.WriteLine("    b. The user wins the game.");
             Console.WriteLine("----------------------------------------------------------------------------------------------------------------------------------\n");
+        }
+
+        public int getNumber()
+        {
+            Random rand = new Random();
+            int number = rand.Next(1, numbersMax+1);
+
+            return number;
+        }
+
+        public void getPattern()
+        {
+            for(int i=0; i<positions; i++)
+            {
+                int randomNumber = getNumber();
+                pattern[i] = randomNumber;
+                //Console.Write(pattern[i] + "");
+            }
         }
     }
 }
