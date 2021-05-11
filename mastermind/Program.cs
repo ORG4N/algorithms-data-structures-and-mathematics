363c929a40471101fa6c8af0d5272b209f498def
ORG4N <callumorgan@gmail.com>
2021-05-11 06:32:31 +0100

    Project finished (history using queue)

--- mastermind/Program.cs
+++ mastermind/Program.cs
@@ -2,18 +2,102 @@
 
 namespace mastermind
 {
+    class Node
+    {
+        public string[] data;
+        public Node next;
+    }
+
+    class Queue
+    {
+        public Node front = null;                   // First item
+        public Node rear = null;                   // Last item
+        public int size = 0;
+    }
+
     class Program
     {
-        public enum States {START, NUMBERS, POSITIONS, PLAYING, CONFIRM, COMPARISON, END, QUIT};
+        public enum States {START, NUMBERS, POSITIONS, PLAYING, COMPARISON, END, QUIT};
         public static States currentState = States.START;
 
-        public int numbersMax = 0;
-        public int positions = 0;
+        public int numbersMax = 0;              // Upper limit of the range of numbers the pattern can include
+        public int positions = 0;               // Size of the pattern
 
-        public int[] pattern = new int[0];
-        public int[] guess = new int[0];
-        public int guessCount = 0;
+        public int[] pattern = new int[0];      // Array for storing random pattern
+        public int[] guess = new int[0];        // Array for storing user's guess
+        public int guessCount = 0;              // Keeps track of the digit position that the user is guessing
 
+        public bool askInput = true;            // Due to program structure, sometimes an input does not need to be requested, so this bool controls that
+
+        Queue history = new Queue();
+
+        // Add an element to the queue
+        public void Enqueue(Queue q, String[] item)
+        {
+
+            Node addItem = new Node();
+            addItem.data = item;
+            addItem.next = null;
+
+            // If the front is null then the queue is empty and therefore when we add our first item it will also be the last
+            if (q.front == null)
+            {
+                q.front = addItem;
+                q.rear = addItem;
+            }
+
+            else
+            {
+                q.rear.next = addItem;
+                q.rear = addItem;
+            }
+
+            q.size++;
+
+        }
+
+        // Pop the first element from the queue
+        public void Dequeue(Queue q)
+        {
+            if (q.front == null)
+            {
+                Console.WriteLine("empty");
+            }
+
+            Object item = q.front.data;
+            q.front = q.front.next;
+            q.size--;
+        }
+
+        // Print first element without removing it
+        public void Peek(Queue q, int i)
+        {
+            if (q.front == null)
+            {
+                Console.WriteLine("empty");
+            }
+
+            Console.WriteLine(" {0}.   {1}               B: {2}  W: {3}", i, q.front.data[0], q.front.data[1], q.front.data[2]);
+        }
+
+        // Print entire queue without deleting it
+        public void Traversal(Queue q)
+        {
+            Node temp = new Node();
+            temp.data = q.front.data;
+            temp.next = q.front.next;
+
+            int i = 1;
+            while (temp != null)
+            {
+                Console.WriteLine(" {0}.   {1}               B: {2}  W: {3}", i, temp.data[0], temp.data[1], temp.data[2]);
+                temp = temp.next;
+                i++;
+            }
+
+        }
+
+        // Program will loop until the program is closed or exited
         static void Main(string[] args)
         {
             Program app = new Program();
@@ -27,14 +111,21 @@ namespace mastermind
 
             while (currentState != States.QUIT)
             {
-                string input = app.readInput();
-                app.processInput(input);
-                app.appLoop();
+                if (app.askInput)
+                {
+                    string input = app.readInput();
+                    app.processInput(input);
+                }
+    
+                app.AppLoop();
             }
         }
 
-        public void appLoop()
+        // Perform a specific action which is determined by the app state
+        public void AppLoop()
         {
+            askInput = true;
+
             if (currentState == States.START)
             {
                 Console.WriteLine("Inputs:");
@@ -43,7 +134,7 @@ namespace mastermind
                 Console.WriteLine(" 'quit'  - to exit the program");
             }
 
-            if (currentState == States.NUMBERS)
+            else if (currentState == States.NUMBERS)
             {
                 Console.WriteLine("\nRange of numbers   : 1 to -");
                 Console.WriteLine("Amount of positions: -\n");
@@ -52,46 +143,62 @@ namespace mastermind
                 Console.WriteLine(" - Input must be an integer between 1 and 8");
             }
 
-            if (currentState == States.POSITIONS)
+            else if (currentState == States.POSITIONS)
             {
                 Console.WriteLine("\nRange of numbers   : 1 to {0}", numbersMax);
                 Console.WriteLine("Amount of positions: -\n");
 
                 Console.WriteLine("Choose the amount of POSITIONS within the secret pattern.");
-                Console.WriteLine(" - Input must be an integer greater than 1");
+                Console.WriteLine(" - Input must be an integer greater than 1");                
             }
 
-            if (currentState == States.COMPARISON)
+            // Calculate the total amount of black and white pegs found in the user's guess
+            else if (currentState == States.COMPARISON)
             {
                 int whitePegs = 0;
                 int blackPegs = 0;
 
-                int[] copy = new int[positions];
+                string[] historyItem = new string[3];
+                string guessString = string.Empty;
 
+                // Convert the guess from an int array to a string and then store in string array
                 for (int i = 0; i < positions; i++)
                 {
-                    copy[i] = pattern[i];
+                    guessString = guessString + guess[i];
                 }
 
+                historyItem[0] = guessString;
+
+
+                // Copy the pattern into another array so that the array can be modified without losing the original pattern
+                int[] posCopy = new int[positions];
+
                 for (int i = 0; i < positions; i++)
                 {
-                    if (guess[i] == pattern[i] && pattern[i] != 0)
+                    posCopy[i] = pattern[i];
+                }
+
+                // Firstly, check for black pegs by checking if the digits at the same indexes of both arrays are the exact same (ignoring zeros)
+                // If they are, replace the digits in the arrays with zeros
+                for (int i = 0; i < positions; i++)
+                {
+                    if (guess[i] == posCopy[i] && posCopy[i] != 0)
                     {
                         blackPegs++;
-                        pattern[i] = 0;
-                        guess[i] = 0;
+                        posCopy[i] = 0;
                     }
                 }
 
+                // Then check for white pegs by comparing each digit within the guess to each index of the pattern (checking if the same digit exists but not in the same index)
+                // If the digit is found then replace both with zeros
                 for (int i = 0; i < positions; i++)
                 { 
                     for (int j = 0; j < positions; j++)
                     {
-                        if (pattern[j] == guess[i] && pattern[j] != 0)
+                        if (posCopy[j] == guess[i] && posCopy[j] != 0)
                         {
                             whitePegs++;
-                            pattern[j] = 0;
-                            guess[i] = 0;
+                            posCopy[i] = 0;
                         }
                     } 
                 }
@@ -99,13 +206,24 @@ namespace mastermind
                 Console.WriteLine("Black pegs: {0}", blackPegs);
                 Console.WriteLine("White pegs: {0}", whitePegs);
 
+                // Convert pegs from int to string via concatentation
+                historyItem[1] = string.Empty + blackPegs;
+                historyItem[2] = string.Empty + whitePegs;
+
+                // Add the guess + black and white pegs to the queue
+                Enqueue(history, historyItem);
+
+                askInput = false;
+
+                // If this statement is true then the player has correctly guessed the pattern
                 if (positions == blackPegs) 
                 { 
                     currentState = States.END;
                     guessCount = 0;
-                    clearGuesses();
+                    ClearGuesses();
                 }
 
+                // Else they should make another guess
                 else 
                 {
                     Console.WriteLine("\n--------------");
@@ -114,25 +232,70 @@ namespace mastermind
 
                     currentState = States.PLAYING;
                     guessCount = 0;
-                    clearGuesses();
+                    ClearGuesses();
                 }
             }
             
-            if (currentState == States.END)
+            // Ask the user if they want to replay
+            else if (currentState == States.END)
             {
-                Console.WriteLine("THE END");
-                currentState = States.QUIT;
+                Console.WriteLine("\n------------");
+                Console.WriteLine("GAME OVER...");
+                Console.WriteLine("------------\n");
+
+                Console.WriteLine("The secret pattern was: ");
+
+                for (int i = 0; i < positions; i++)
+                {
+                    Console.Write("{0} ", pattern[i]);
+                }
+
+                Console.WriteLine("\n\nGuess History:");
+
+                int j = 1;
+                while (history.size > 0)
+                {
+                    Peek(history, j);
+                    Dequeue(history);
+                    j++;
+                }
+
+                Console.WriteLine("\n\nDo you want to replay?");
+                Console.WriteLine(" 1. Yes");
+                Console.WriteLine(" 2. No");
+
+                askInput = false;
+
+                if (Confirm() == true)
+                {
+                    currentState = States.START;
+                }
+
+                else
+                {
+                    currentState = States.QUIT;
+                }
             }
 
-            if (currentState == States.PLAYING)
+            // Writing text to the screen to display necessary information whilst the player is playing
+            else if (currentState == States.PLAYING)
             {
-                if (guessCount < positions)
+                if (guessCount < positions) // Player is still guessing
                 {
-                    //Console.Clear();
                     Console.WriteLine("\nRange of numbers   : 1 to {0}", numbersMax);
                     Console.WriteLine("Amount of positions: {0}\n", positions);
 
-                    Console.WriteLine("Player A has created a pattern of {0} size...\n", positions);
+                    if (history.size == 0)
+                    {
+                        Console.WriteLine("Player A has created a pattern of {0} size...\n", positions);
+                    }
+
+                    if (history.size > 0)
+                    {
+                        Console.WriteLine("Previous guesses: ");
+                        Traversal(history);// write all of the contents of the queue
+                        Console.WriteLine();
+                    }
 
                     Console.Write("Position  : ");
                     for (int i = 0; i < positions; i++)
@@ -149,11 +312,9 @@ namespace mastermind
                     Console.WriteLine("\n\nMake guess for position: {0}", guessCount + 1);
                 }
 
-                else
+                else // Player has finished guessing
                 {
                     Console.Clear();
-
-                    currentState = States.CONFIRM;
 
                     Console.Write("Position  : ");
                     for (int i = 0; i < positions; i++)
@@ -170,6 +331,23 @@ namespace mastermind
                     Console.WriteLine("\n\nConfirm pattern?");
                     Console.WriteLine(" 1. Yes");
                     Console.WriteLine(" 2. No");
+
+                    askInput = false;
+
+                    // Submit the guess
+                    if(Confirm() == true)
+                    {
+                        currentState = States.COMPARISON;
+                    }
+
+                    // Restart guessing and reset game state and clear arrays
+                    else
+                    {
+                        Console.Clear();
+                        currentState = States.PLAYING;
+                        guessCount = 0;
+                        ClearGuesses();
+                    }
                 }
             }
         }
@@ -185,6 +363,7 @@ namespace mastermind
 
 
         // Use the input to determine if the program should do anything (i.e. change a state, call a method)
+        // Primarily used to see if the input meets a specific criteria: such as being an integer or between a specific range
         public void processInput(string input)
         {
             if (input == "rules" && currentState == States.START) { printRules(); }
@@ -197,6 +376,7 @@ namespace mastermind
                 Console.Clear();
             }
 
+            // Ensure that the user inputs a number within the control criteria
             else if (currentState == States.NUMBERS)
             {
                 int num;
@@ -217,6 +397,7 @@ namespace mastermind
                 else {Console.WriteLine("\nINVALID INPUT - Input must be a number and between 1 and 8\n");}
             }
 
+            // Ask user for pattern size and then generate a random pattern based on size
             else if (currentState == States.POSITIONS)
             {
                 int num;
@@ -230,7 +411,7 @@ namespace mastermind
                         pattern = new int[positions];
                         guess = new int[positions];
 
-                        getPattern();
+                        GetPattern();
 
                         currentState = States.PLAYING;
                     }
@@ -241,12 +422,13 @@ namespace mastermind
                 else {Console.WriteLine("\nINVALID INPUT - Input must be a number and greater than 1\n");}
             }
 
+            // Ensure guesses are greater than 0 and less than the upper limit digit
             else if (currentState == States.PLAYING)
             {
                 int num;
                 bool isNumber = Int32.TryParse(input, out num);
 
-                if (guessCount <= numbersMax)
+                if (guessCount <= positions)
                 {
                     if (isNumber && num > 0 && num <= numbersMax)
                     {
@@ -261,28 +443,6 @@ namespace mastermind
 
                 }
             }
-
-            else if (currentState == States.CONFIRM)
-            {
-                if(input == "yes" || input == "1")
-                {
-                    currentState = States.COMPARISON;
-                }
-
-                else if (input == "no" || input == "2")
-                {
-                    Console.Clear();
-                    currentState = States.PLAYING;
-                    guessCount = 0;
-                    clearGuesses();
-                }
-
-                else
-                {
-                    Console.WriteLine("Invalid Input...");
-                }
-            }
-
         }
 
 
@@ -305,6 +465,7 @@ namespace mastermind
             Console.WriteLine("----------------------------------------------------------------------------------------------------------------------------------\n");
         }
 
+        // Use random class to get a random number
         public int getNumber()
         {
             Random rand = new Random();
@@ -313,23 +474,48 @@ namespace mastermind
             return number;
         }
 
-        public void getPattern()
+        // Make random pattern of N size
+        public void GetPattern()
         {
             for(int i=0; i<positions; i++)
             {
                 int randomNumber = getNumber();
                 pattern[i] = randomNumber;
-                Console.Write(pattern[i] + "");
             }
         }
 
-        public void clearGuesses()
+        // Clear the array by setting all values within to 0
+        public void ClearGuesses()
         {
             for (int i = 0; i < positions; i++)
             {
                 guess[i] = 0;
             }
 
+        }
+
+        // Reusable code to take an input and use to assess a yes or no input
+        public bool Confirm()
+        {
+            while (true)
+            {
+                string input = readInput();
+
+                if (input == "yes" || input == "1")
+                {
+                    return true;
+                }
+
+                else if (input == "no" || input == "2")
+                {
+                    return false;
+                }
+
+                else
+                {
+                    Console.WriteLine("Invalid Input...");
+                }
+            }
         }
     }
 }
